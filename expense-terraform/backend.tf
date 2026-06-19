80360482288654ee1899a2c716d49597f817271e
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-06-20 01:47:21 +0200

    test

--- expense-terraform/backend.tf
+++ expense-terraform/backend.tf
@@ -1,11 +1,8 @@
 terraform {
   backend "s3" {
 
-bucket         = var.bucket_name
-    key            = var.key_name
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = var.dynamodb_table # This line enables state locking    
+    
+ # This line enables state locking    
     # This line enables state locking
     
   }
--- expense-terraform/env-dev/state.tfvars
+++ expense-terraform/env-dev/state.tfvars
@@ -1,3 +1,4 @@
     bucket         = "techkarthi170626"
     key            = "dev/terraform.tfstate"
     dynamodb_table = "terraform-lock-table" 
+    region         = "us-east-1"
