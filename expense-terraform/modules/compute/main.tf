82236dac357a037533096be350639215d8c50d4f
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-06-20 07:35:07 +0200

    test

--- expense-terraform/modules/compute/main.tf
+++ expense-terraform/modules/compute/main.tf
@@ -30,4 +30,12 @@ resource "aws_security_group" "common" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # "-1" means all protocols
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
